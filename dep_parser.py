bb1f3d7714acd411f48d48f6b76e2547afcc54d7
derek <768248096@qq.com>
2021-08-23 14:06:02 +0800

    Update dep_parser.py

--- dep_parser.py
+++ dep_parser.py
@@ -1,6 +1,4 @@
 import copy
-import spacy
-import re
 
 class DepInstanceParser():
     def __init__(self, basicDependencies, tokens=[]):
@@ -162,51 +160,3 @@ class DepInstanceParser():
                     dep_path_type_matrix[right_index][left_index] = first_order_dep_type_matrix[right_index][left_index]
         return dep_path_adj_matrix, dep_path_type_matrix
 
-
-class DepTreeParser():
-    def __init__(self):
-        pass
-
-    def parsing(self, sentence):
-        pass
-
-class SpaCyDepTreeParser(DepTreeParser):
-    def __init__(self):
-        self.nlp = spacy.load("en_core_web_sm")
-
-    def parsing(self, sentence):
-        doc = self.nlp(sentence)
-        basicDependencies = []
-        tokens = []
-        for i, token in enumerate(doc):
-            if token.dep_ == "ROOT":
-                basicDependencies.append({
-                    "dep": token.dep_,
-                    "governor": 0,
-                    "governorGloss": "ROOT",
-                    "dependent": token.i+1,
-                    "dependentGloss": token.text
-                })
-            else:
-                basicDependencies.append({
-                    "dep": token.dep_,
-                    "governor": token.head.i+1,
-                    "governorGloss": token.head.text,
-                    "dependent": token.i+1,
-                    "dependentGloss": token.text
-                })
-            tokens.append({
-                "index": token.i,
-                "word": token.text,
-                "originalText": token.text
-            })
-        return {
-            "sentences": [
-                {
-                    "index": 0,
-                    "line": 1,
-                    "basicDependencies": basicDependencies,
-                    "tokens": tokens
-                }
-            ]
-        }
